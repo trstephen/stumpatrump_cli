22f7b457253631091dfa7d50655267f677c61cea
Tyler Stephen <tyler.stephen@gmail.com>
2016-11-23 13:06:21 -0800

    Get responses from service
    - Requests and displays data from service running on localhost
    - No config file yet :/
    - Add crummy error handling that catches, not handles error

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const axios = require('axios');
 let correctGuessCount = 0;
 let totalGames = 0;
 
@@ -21,57 +22,67 @@ const questions = {
   ],
 };
 
+const service = axios.create({
+  baseURL: 'http://localhost:3000',
+  timeout: 5000,
+});
+
 function getRandomTweet() {
-  // TODO: Use GET /tweets
-  const id = Math.floor(Math.random() * 10);
-  const text = `Random tweet with ID ${id}`;
+  return service.get('/tweets')
+    .then(resp => {
+      if (resp.status !== 200) throw new Error('Non-200 response from Twitter API');
 
-  return { id, text };
-}
-
-function getTweetAuthor(tweetId) {
-  // TODO: Use GET /tweets/:tweet_id
-  return {
-    screen_name: `Mr. ${tweetId}`,
-    isTrump: Math.random() > 0.5,
-  };
-}
-
-function game() {
-  const tweet = getRandomTweet();
-  console.log('Here’s a random tweet');
-  console.log(tweet.text);
-
-  inquirer.prompt(questions.tweetAuthenticity)
-    .then((answers) => [
-      answers,
-      getTweetAuthor(tweet.id),
-    ])
-    .then(([answers, tweetAuthor]) => {
-      const isCorrectGuess = answers.isTrumpGuess === tweetAuthor.isTrump;
-
-      console.log(`The author is ${tweetAuthor.screen_name}`);
-
-      const feedbackMsg = isCorrectGuess ? 'Beautiful. Amazing.' : 'Sad!';
-      console.log(feedbackMsg);
-
-      totalGames += 1;
-      if (isCorrectGuess) correctGuessCount += 1;
-
-      console.log(`Your score is ${correctGuessCount}/${totalGames}`);
-    })
-    .then(() => {
-      inquirer.prompt(questions.playAgain)
-        .then((answers) => {
-          if (answers.playAgain) {
-            game();
-          } else {
-            console.log(`Your final score is ${correctGuessCount}/${totalGames}`);
-            console.log('Go out there and make America great!!');
-          }
-        });
+      return resp.data;
     });
 }
 
+function getTweetAuthor(tweetId) {
+  return service.get(`/tweets/${tweetId}`)
+    .then(resp => {
+      if (resp.status !== 200) throw new Error('Non-200 response from Twitter API');
+
+      return resp.data;
+    });
+}
+
+function game() {
+  getRandomTweet()
+    .then(tweet => {
+      console.log(tweet.text);
+
+      return tweet;
+    })
+    .then(tweet => getTweetAuthor(tweet.id))
+    .then(tweetAuthor => {
+      inquirer.prompt(questions.tweetAuthenticity)
+        .then(answers => {
+          const isCorrectGuess = answers.isTrumpGuess === tweetAuthor.isTrump;
+
+          console.log(`The author is ${tweetAuthor.screenName}`);
+
+          const feedbackMsg = isCorrectGuess ? 'Beautiful. Amazing.' : 'Sad!';
+          console.log(feedbackMsg);
+
+          totalGames += 1;
+          if (isCorrectGuess) correctGuessCount += 1;
+
+          console.log(`Your score is ${correctGuessCount}/${totalGames}`);
+        })
+        .then(() => {
+          inquirer.prompt(questions.playAgain)
+            .then((answers) => {
+              if (answers.playAgain) {
+                game();
+              } else {
+                console.log(`Your final score is ${correctGuessCount}/${totalGames}`);
+                console.log('Go out there and make America great!!');
+              }
+            });
+        });
+    })
+    .catch(console.error);
+}
+
 console.log('Welcome to Stump-A-Trump~');
+console.log('Here’s a random tweet');
 game();
